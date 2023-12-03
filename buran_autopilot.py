965f6ec9bb1d1ca7fff7fc2cc31ce30af7f612e8
MrDemmy <66142897+MrDemmy@users.noreply.github.com>
2023-12-03 19:41:30 +0300

    fixed autopilots and ship

--- buran_autopilot.py
+++ buran_autopilot.py
@@ -4,7 +4,7 @@ import krpc
 
 target_altitude = 150000
 
-conn = krpc.connect(name='Buran')
+conn = krpc.connect(name='Buran Launch')
 vessel = conn.space_center.active_vessel
 
 # Set up streams for telemetry
@@ -38,10 +38,6 @@ print('Target apoapsis reached')
 vessel.control.activate_next_stage()
 vessel.control.throttle = 0.0
 
-# Wait until out of atmosphere
-print('Coasting out of atmosphere')
-while altitude() < 70500:
-    pass
 
 # Plan circularization burn (using vis-viva equation)
 print('Planning circularization burn')
@@ -59,7 +55,7 @@ F = vessel.available_thrust
 Isp = vessel.specific_impulse * 9.82
 m0 = vessel.mass
 m1 = m0 / math.exp(delta_v/Isp)
-flow_rate = F / Isp / 5
+flow_rate = F / Isp / 2.6
 burn_time = (m0 - m1) / flow_rate
 
 # Orientate ship
@@ -68,7 +64,72 @@ vessel.auto_pilot.reference_frame = node.reference_frame
 vessel.auto_pilot.target_direction = (0, 1, 0)
 vessel.auto_pilot.wait()
 
+
+# Wait until out of atmosphere
+print('Coasting out of atmosphere')
+while altitude() < 70500:
+    pass
+
 # Wait until burn
+print('Waiting until circularization burn')
+burn_ut = ut() + vessel.orbit.time_to_apoapsis - (burn_time/2.)
+lead_time = 15
+conn.space_center.warp_to(burn_ut - lead_time)
+
+# Execute burn
+print('Ready to execute burn')
+time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, 'time_to_apoapsis')
+while time_to_apoapsis() - (burn_time/2.) > 0:
+    pass
+print('Executing burn')
+vessel.control.throttle = 1 / 2.6
+
+
+time.sleep(burn_time - 0.1)
+
+print('Fine tuning')
+vessel.control.throttle = 0.05
+remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
+while remaining_burn()[1] > 4:
+    pass
+vessel.control.throttle = 0.0
+node.remove()
+
+vessel.auto_pilot.disengage()
+
+vessel.control.activate_next_stage()
+
+conn.space_center.active_vessel = conn.space_center.vessels[-1]
+vessel = conn.space_center.active_vessel
+
+vessel.control.throttle = 0.0
+
+time.sleep(10)
+
+r = vessel.orbit.apoapsis
+a1 = vessel.orbit.semi_major_axis
+a2 = r
+v1 = math.sqrt(mu*((2./r)-(1./a1)))
+v2 = math.sqrt(mu*((2./r)-(1./a2)))
+delta_v = v2 - v1
+node = vessel.control.add_node(ut() + vessel.orbit.time_to_apoapsis, prograde=delta_v)
+
+# Calculate burn time (using rocket equation)
+F = vessel.available_thrust
+Isp = vessel.specific_impulse * 9.82
+m0 = vessel.mass
+m1 = m0 / math.exp(delta_v/Isp)
+flow_rate = F / Isp
+burn_time = (m0 - m1) / flow_rate
+print(F, Isp, m0, burn_time)
+
+vessel.auto_pilot.engage()
+
+vessel.auto_pilot.reference_frame = node.reference_frame
+vessel.auto_pilot.target_direction = (0, 1, 0)
+vessel.auto_pilot.wait()
+
+
 print('Waiting until circularization burn')
 burn_ut = ut() + vessel.orbit.time_to_apoapsis - (burn_time/2.)
 lead_time = 5
@@ -80,24 +141,12 @@ time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, 'time_to_apoapsis')
 while time_to_apoapsis() - (burn_time/2.) > 0:
     pass
 print('Executing burn')
-vessel.control.throttle = 0.2
+vessel.control.throttle = 1
 
-
-time.sleep(burn_time - 0.1)
-
-print(vessel.orbit.semi_major_axis, conn.space_center.target_vessel.orbit.semi_major_axis, sep='\n')
-
-print('Fine tuning')
-vessel.control.throttle = 0.05
 remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
-while remaining_burn()[1] > 4:
+while remaining_burn()[1] > 1:
     pass
 vessel.control.throttle = 0.0
 node.remove()
-
-vessel.control.activate_next_stage()
-conn.space_center.active_vessel = conn.space_center.vessels[-1]
-vessel = conn.space_center.active_vessel
-
 
 print('Launch complete')
--- hohmann_auto.py
+++ hohmann_auto.py
@@ -1,12 +1,18 @@
+import math
+import time
+
 import krpc
 
-conn = krpc.connect(name='Buran')
+conn = krpc.connect(name='Buran Hohmann')
 vessel = conn.space_center.active_vessel
 
 ut = conn.add_stream(getattr, conn.space_center, 'ut')
 altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
 apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
 target = conn.space_center.target_vessel
+
+vessel.auto_pilot.engage()
+vessel.auto_pilot.target_direction = (0, 0, 1)
 
 PI = 3.141592653589793
 rad2deg = 180 / PI
@@ -25,13 +31,16 @@ phase_ang_RoC = abs(target_ang_speed - vessel_ang_speed)
 rf = vessel.orbit.body.reference_frame
 ang = target.flight(rf).longitude - vessel.flight(rf).longitude
 
+conn.space_center.rails_warp_factor = 6
+close_enough = False
 # # # КОСТЫЛИ
 while abs(ang - required_angle) > 0.01:
     ang = target.flight(rf).longitude - vessel.flight(rf).longitude
+    if not close_enough and abs(ang - required_angle) < (PI / 2 * rad2deg):
+        conn.space_center.rails_warp_factor = 4
+        close_enough = True
     if abs(ang - required_angle) < 3 and conn.space_center.rails_warp_factor > 0:
         conn.space_center.rails_warp_factor = 0
-    print(ang)
-conn.krpc.paused = True
 print('READY')
 # # #
 
@@ -44,4 +53,26 @@ delta_v1 = (mu / vessel.orbit.radius) ** 0.5 * (((2 * target.orbit.radius) / (ta
 # Circularization burn
 delta_v2 = (mu / target.orbit.radius) ** 0.5 * (1 - ((2 * vessel.orbit.radius) / (target.orbit.radius + vessel.orbit.radius)) ** 0.5)
 
-vessel.control.add_node(ut() + abs(ang - required_angle) / vessel_ang_speed - abs(ang - required_angle) / target_ang_speed, prograde=delta_v1)
+node = vessel.control.add_node(ut() + abs(ang - required_angle) / vessel_ang_speed - abs(ang - required_angle) / target_ang_speed, prograde=delta_v1)
+
+
+# Calculate burn time (using rocket equation)
+F = vessel.available_thrust
+Isp = vessel.specific_impulse * 9.82
+m0 = vessel.mass
+m1 = m0 / math.exp(delta_v1/Isp)
+flow_rate = F / Isp / 4
+burn_time = (m0 - m1) / flow_rate
+
+# Orientate ship
+vessel.auto_pilot.reference_frame = node.reference_frame
+vessel.auto_pilot.target_direction = (0, 1, 0)
+vessel.auto_pilot.wait()
+
+vessel.control.throttle = 1
+
+remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
+while remaining_burn()[1] > 1:
+    pass
+vessel.control.throttle = 0.0
+node.remove()
