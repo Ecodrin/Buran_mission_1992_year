14bb05e3c70bc7741a2f42c7ee2fedf5d3ff7d58
Ecodrin <matvfo@ya.ru>
2023-12-10 22:08:24 +0300

    update readme

--- src/scripts.py
+++ src/scripts.py
@@ -26,11 +26,10 @@ def first_orbit():
     thrust_of_the_first_stage = 9806.65 * 2600
     table = {'Высота': [], 'Скорость': [], 'Ускорение по x': [], 'Ускорение по y':[]}
     acceleration = Vector()
-    speed = Vector()
+    velocity = Vector()
     alpha = 90
     gg = 3.5 * 10 ** 12
     m_0 = 240_000
-    coordinate = Vector(0, 600_000)
     minus_topl = [sqrt(i) for i in range(10000)]
 
     pl = {0: 1.225, 2500: 0.898, 5000: 0.642, 7500: 0.446,
@@ -39,30 +38,30 @@ def first_orbit():
     t = 0
     x = [0]
     y = [600_000]
-    spped_first_cosmo = sqrt(gg / (600_000 + 150_000))
+    speed_first_cosmo = sqrt(gg / (600_000 + 150_000))
     while height < 150_000:
         f_thrust = thrust_of_the_first_stage
         f_gravity = gg * m_0 / ((600_000 + height) ** 2)
-        f_resistance = (resistance_coefficient * (pi * 9 * (18 + 10)) * (speed.y ** 2)
+        f_resistance = (resistance_coefficient * (pi * 9 * (18 + 10)) * (velocity.y ** 2)
                         * (pl[int(height // 10_000 * 10_000)] if height <= 40000 else 0) / 2)
         acceleration.y = (f_thrust - f_resistance - f_gravity) * sin(radians(alpha)) / m_0
-        print(acceleration.x, acceleration.y, sep=" ------ ")
-        height += speed.y + acceleration.y / 2
-        y.append(y[-1] + speed.y + acceleration.y / 2)
-        speed.y = acceleration.y + speed.y
+        #print(acceleration.x, acceleration.y, sep=" ------ ")
+        height += velocity.y + acceleration.y / 2
+        y.append(y[-1] + velocity.y + acceleration.y / 2)
+        velocity.y = acceleration.y + velocity.y
         table['Высота'].append(height)
         acceleration.x = (f_thrust - f_resistance - f_gravity) * cos(radians(alpha)) / m_0
-        speed.x = acceleration.x + speed.x
-        table['Скорость'].append(sqrt(speed.x ** 2 + speed.y ** 2))
+        velocity.x = acceleration.x + velocity.x
+        table['Скорость'].append(sqrt(velocity.x ** 2 + velocity.y ** 2))
         table['Ускорение по x'].append(acceleration.x)
         table['Ускорение по y'].append(acceleration.y)
         m_0 -= minus_topl[t]
-        full_speed = sqrt(speed.x ** 2 + speed.y ** 2)
+        full_speed = sqrt(velocity.x ** 2 + velocity.y ** 2)
         t += 1
-        x.append(x[-1] + speed.x + acceleration.x)
-        if full_speed > spped_first_cosmo:
+        x.append(x[-1] + velocity.x + acceleration.x)
+        if full_speed > speed_first_cosmo:
             thrust_of_the_first_stage = 0
-        if height > 6000 and alpha > 2:
+        if height > 60000 and alpha > 2:
             alpha -= 2
         time_rise.append(t)
     plt.grid(color='black', linestyle='-', linewidth=1)
@@ -71,7 +70,7 @@ def first_orbit():
     plt.xlabel('Время в секундах')
     plt.ylabel('Высота в метрах')
     plt.show()
-    plt.grid(color='b', linestyle='-', linewidth=1)
+    plt.grid(color='black', linestyle='-', linewidth=1)
     plt.plot(time_rise, table['Скорость'])
     plt.title('Зависимость скорости от времени')
     plt.xlabel("Время в секундах")
@@ -79,8 +78,7 @@ def first_orbit():
     plt.show()
     plt.grid(color='black', linestyle='-', linewidth=1)
     table = pd.DataFrame(table)
-    print(table)
-    table.to_csv("report")
+    table.to_csv("./report.csv")
     second_orbit(acceleration, height, m_0)
     plt.plot(x, y)
     plt.title("Координатная зависимость")
@@ -88,6 +86,8 @@ def first_orbit():
     plt.ylabel("Координаты по Y")
     plt.show()
     plt.grid(color='black', linestyle='-', linewidth=2)
+    print(table)
+    print("All good. Check out report.csv")
 
 
 def second_orbit(acceleration, height, m_0):
